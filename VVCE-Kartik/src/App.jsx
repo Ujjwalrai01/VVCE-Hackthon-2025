c17b6b9bc7d36ea859e986c05666cfdc13cd912a
Ujjwal Rai <urai31382@gmail.com>
2025-05-05 20:55:52 +0530

    VVCE

--- VVCE-Kartik/src/App.jsx
+++ VVCE-Kartik/src/App.jsx
@@ -1,24 +1,24 @@
 
-import { useState } from 'react'
-import AppRouter from "./Route/AppRouter";
-import './App.css'
-import Login from './Pages/Login';
-import UserDashboard from './Pages/UserDashboard/UserDashboard';
-import Home from "./Layouts/LandingPage";
+// import { useState } from 'react'
+// import AppRouter from "./Route/AppRouter";
+// import './App.css'
+// import Login from './Pages/Login';
+// import UserDashboard from './Pages/UserDashboard/UserDashboard';
+// import Home from "./Layouts/LandingPage";
 
 function App() {
 
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+     <BrowserRouter>
+     <Routes> 
+      {/* <Route path="/" element={<Home />} /> */}
+      {/* <Route path="/login" element={<Login />} /> */}
+      {/* <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<LandingPage />} />
       <Route path="/userdashboard" element={<UserDashboard />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
-  </BrowserRouter>
+      <Route path="/admin" element={<Admin />} /> */}
+    </Routes> 
+   </BrowserRouter> 
   )
 }
 
