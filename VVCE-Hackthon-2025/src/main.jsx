6cc6de7518daf7f29ae2a1939678d8df6db14a2f
Ujjwal Rai <urai31382@gmail.com>
2025-05-11 17:52:08 +0530

    VVCE

--- VVCE-Hackthon-2025/src/main.jsx
+++ VVCE-Hackthon-2025/src/main.jsx
@@ -1,10 +1,30 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import App from './App.jsx'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom';
+// import './index.css'
+// import App from './App.jsx'
+// import './i18n'; 
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-     <App />
-  </BrowserRouter>
-)
+// createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//      <App />
+//   </BrowserRouter>
+// )
+
+
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './store/Store'; 
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
