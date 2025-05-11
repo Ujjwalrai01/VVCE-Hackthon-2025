ae3a9b64eae17317fdc5f48f7558eb367d855210
Ujjwal Rai <urai31382@gmail.com>
2025-05-11 17:53:42 +0530

    VVCE

--- VVCE-Hackthon-2025/src/App.jsx
+++ VVCE-Hackthon-2025/src/App.jsx
@@ -5,21 +5,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
 import LoginPage from './Pages/LoginPage';
 import SignupPage from './Pages/SignupPage';
-import DashboardPage from './Pages/DashboardPage';
+// import DashboardPage from './Pages/DashboardPage';
 import AdminPanel from './Pages/AdminPanel';
 import ResultPage from './Pages/ResultPage';
 import UploadForm from './Pages/UploadForm';
+import Render from './layouts/Render';
 
 function App() {
+  
   return (
     // <BrowserRouter>
       <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path='/loginpage' element={<LoginPage/>}/>
-      <Route path='/signuppage' element={<SignupPage/>}/>
-      <Route path='/dashboardpage' element={<DashboardPage/>}/>
-      <Route path='/adminpanel' element={<AdminPanel/>}/>
-      <Route path='/resultpage' element={<ResultPage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/signup' element={<SignupPage/>}/>
+      <Route path='/dashboard' element={<Render/>}/>
+      {/* <Route path='/admin' element={<AdminPanel/>}/> */}
+      <Route path='/result/:docId' element={<ResultPage/>}/>
       <Route path='/uploadform' element={<UploadForm/>}/>
 
       {/* <Route path="/login" element={<Login />} />
